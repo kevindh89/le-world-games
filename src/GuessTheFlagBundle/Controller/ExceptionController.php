07cfc912b51e54f7f5b27d839a1620122a487473
Kevin de Heer <k.deheer@simgroep.nl>
2019-09-19 16:46:59 +0200

    Adds a custom exception page for 500 exceptions

---
+++ src/GuessTheFlagBundle/Controller/ExceptionController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GuessTheFlagBundle\Controller;
+
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+
+class ExceptionController extends \Symfony\Bundle\TwigBundle\Controller\ExceptionController
+{
+    public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null)
+    {
+        $currentContent = $this->getAndCleanOutputBuffering($request->headers->get('X-Php-Ob-Level', -1));
+        $showException = $request->attributes->get('showException', $this->debug); // As opposed to an additional parameter, this maintains BC
+
+        $code = $exception->getStatusCode();
+
+        return new Response($this->twig->render(
+            (string) $this->findTemplate($request, $request->getRequestFormat(), $code, $showException),
+            [
+                'status_code' => $code,
+                'status_text' => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : '',
+                'exception' => $exception,
+                'logger' => $logger,
+                'currentContent' => $currentContent,
+                'theme' => ['red', 'orange', 'blue'][rand(0, 2)],
+            ]
+        ), 200, ['Content-Type' => $request->getMimeType($request->getRequestFormat()) ?: 'text/html']);
+    }
+}
